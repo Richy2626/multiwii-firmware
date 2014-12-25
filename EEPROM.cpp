05d4b1acd3d367fdb4d70d7381b8cb4fe337877f
offbyone <offbyone@gmail.com>
2014-12-25 12:41:39 -0500

    Tweaks to default RC rates

--- EEPROM.cpp
+++ EEPROM.cpp
@@ -171,9 +171,9 @@ void LoadDefaults() {
 
     conf.pid[PIDVEL].P8 = 0;      conf.pid[PIDVEL].I8 = 0;    conf.pid[PIDVEL].D8 = 0;
 
-    conf.rcRate8 = 90; conf.rcExpo8 = 65;
-    conf.rollPitchRate = 0;
-    conf.yawRate = 0;
+    conf.rcRate8 = 140; conf.rcExpo8 = 65;
+    conf.rollPitchRate = 30;
+    conf.yawRate = 35;
     conf.dynThrPID = 0;
     conf.thrMid8 = 50; conf.thrExpo8 = 0;
     for(i=0;i<CHECKBOXITEMS;i++) {conf.activate[i] = 0;}
